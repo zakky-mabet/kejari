d24807be24145a8b76185a499c2b0618e7d8fbcd
Muhamad Zakky <muhamadzakky45@gmail.com>
2018-02-17 02:03:31 +0700

    update bahasa indonesia

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,8 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+//$autoload['libraries'] = array();
+$autoload['libraries'] = array('database','table','form_validation','pagination','upload','user_agent','template','page_title','breadcrumbs','session','slug');
 
 /*
 | -------------------------------------------------------------------
@@ -89,8 +90,8 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('indonesia');
 
+$autoload['helper'] = array('url','form','file','html','cookie','language','indonesia');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Config files
--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url']  = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http")."://".$_SERVER['HTTP_HOST'].str_replace(basename($_SERVER['SCRIPT_NAME']),"", $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
@@ -76,7 +76,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']	= 'indonesia';
 
 /*
 |--------------------------------------------------------------------------
